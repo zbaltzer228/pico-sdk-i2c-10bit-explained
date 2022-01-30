39bd108e0144a2ec3a40532b54272ec40d053f42
zbaltzer228 <53534477+zbaltzer228@users.noreply.github.com>
2022-01-29 19:50:03 -0500

    Update RP2040_10bit_i2c_host.c

--- Examples/RP2040_10bit_i2c_host/RP2040_10bit_i2c_host.c
+++ Examples/RP2040_10bit_i2c_host/RP2040_10bit_i2c_host.c
@@ -2,14 +2,9 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
-
-
 
 int main()
 {
@@ -17,7 +12,7 @@ int main()
     char receivedMessage[3];
     stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
+    // I2C Initialisation. Using it at 100Khz with 10-bit addressing.
     i2c_init_10bit(I2C_PORT, 100*1000);
 
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
